eb603eb4b4d1396296abf61178d2f7b4ea06570c
sellenth <sellenth@oregonstate.edu>
2019-03-06 14:32:56 -0800

    gets REST data

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  request.get('https://ign-apis.herokuapp.com/content?count=1', function(err, resp, body) {
+    if (!err && resp.statusCode == 200) {
+      var locals = JSON.parse(body);
+      res.send(locals);
+    }
+  });
+  //    res.send('respond with a resource');
 });
 
 module.exports = router;
